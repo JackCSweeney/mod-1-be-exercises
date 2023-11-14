83fd0aa7e6bab79093c8394ef036ea2f7283cec7
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-14 09:33:03 -0800

    Add new tests to wizard_spec.rb

--- ruby_exercises/mythical-creatures/spec/wizard_spec.rb
+++ ruby_exercises/mythical-creatures/spec/wizard_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require './lib/wizard'
+require '../lib/wizard'
 
 RSpec.describe Wizard do
   it 'has a name' do
@@ -34,19 +34,31 @@ RSpec.describe Wizard do
 
   it 'starts rested' do
     # create wizard
+    wizard = Wizard.new('Doug')
+
     # .rested? returns true
+    expect(wizard.rested?).to eq true
   end
 
   it 'can cast spells' do
     # create wizard
+    wizard = Wizard.new('Doug')
+
     # .cast returns "MAGIC MISSILE!"
+    expect(wizard.cast).to eq "MAGIC MISSILE!"
   end
 
   it 'gets tired after casting three spells' do
     # create wizard
+    wizard = Wizard.new('Doug')
     # casts spell twice
+    wizard.cast
+    wizard.cast
     # check if wizard is rested
+    expect(wizard.rested?).to eq true
     # casts spell
+    wizard.cast
     # check wizard is not rested
+    expect(wizard.rested?).to eq false
   end
 end
