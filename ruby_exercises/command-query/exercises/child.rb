8e8f9916024f0041de0ea87be7f7ddbdf6dde5c4
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-20 08:47:17 -0800

    Create and add code to child.rb to pass tests

---
+++ ruby_exercises/command-query/exercises/child.rb
@@ -0,0 +1,15 @@
+class Child
+    attr_accessor :name, :age
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+
+    def age
+        @age
+    end
+
+    def name
+        @name
+    end
+end
