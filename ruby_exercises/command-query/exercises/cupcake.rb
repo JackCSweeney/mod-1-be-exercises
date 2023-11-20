d3818696c039b331179a553eb7ec8adf50b5742c
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-20 08:49:49 -0800

    Create and add code to cupcake.rb to pass tests

---
+++ ruby_exercises/command-query/exercises/cupcake.rb
@@ -0,0 +1,15 @@
+class Cupcake
+    attr_accessor :flavor, :sugar
+    def initialize(flavor, sugar)
+        @flavor = flavor
+        @sugar = sugar
+    end
+
+    def flavor
+        @flavor
+    end
+
+    def sugar
+        @sugar
+    end
+end
