a1bbd139da50be38637f877b2d3cf0a8f1afec19
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-28 20:30:17 -0800

    Add additional tests to venue_spec.rb

--- practice_independent_challenges/week1/venue/spec/venue_spec.rb
+++ practice_independent_challenges/week1/venue/spec/venue_spec.rb
@@ -4,25 +4,25 @@ require './lib/venue'
 describe Venue do
   describe '#initialize' do
     it 'is a venue' do
-      skip
+      
       venue = Venue.new('Bluebird', 4)
       expect(venue).to be_a Venue
     end
 
     it 'can read the name' do
-      skip
+      
       venue = Venue.new('Bluebird', 4)
       expect(venue.name).to eq 'Bluebird'
     end
 
     it 'can read the capacity' do
-      skip
+      
       venue = Venue.new('Bluebird', 4)
       expect(venue.capacity).to eq 4
     end
 
     it 'has no patrons by default' do
-      skip
+      
       venue = Venue.new('Bluebird', 4)
       expect(venue.patrons).to eq []
     end
@@ -32,7 +32,7 @@ describe Venue do
 
   describe '#add_patron' do
     it 'returns a list of patrons' do
-      skip
+      
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
@@ -44,7 +44,7 @@ describe Venue do
 
   describe '#yell_at_patrons' do
     it 'returns a list of uppercased names' do
-      skip
+      
       venue = Venue.new('Bluebird', 4)
       venue.add_patron('Mike')
       venue.add_patron('Megan')
@@ -52,4 +52,29 @@ describe Venue do
       expect(venue.yell_at_patrons).to eq ['MIKE', 'MEGAN', 'BOB']
     end
   end
+
+  # Iteration 3
+
+  it 'can be over capacity' do
+      venue = Venue.new('Bluebird', 4)
+      venue.add_patron('Mike')
+      venue.add_patron('Megan')
+      venue.add_patron('Bob')
+
+      expect(venue.over_capacity?).to be(false)
+  end
+
+  it 'can kick out patrons' do
+    venue = Venue.new('Bluebird', 4)
+    venue.add_patron('Mike')
+    venue.add_patron('Megan')
+    venue.add_patron('Bob')
+    venue.add_patron('James')
+    venue.add_patron('Cat')
+
+    venue.kick_out
+
+    expect(venue.over_capacity?).to be false
+  end
+
 end
