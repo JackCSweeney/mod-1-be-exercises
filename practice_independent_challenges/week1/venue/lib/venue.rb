4105832f3fb12ce1e1b489dc0919271cc5283e34
Jack Sweeney <jackcsweeney@gmail.com>
2023-11-28 20:29:49 -0800

    Add code to venue.rb to make tests pass

--- practice_independent_challenges/week1/venue/lib/venue.rb
+++ practice_independent_challenges/week1/venue/lib/venue.rb
@@ -1,2 +1,32 @@
 class Venue
+
+    attr_reader :name, :capacity, :patrons
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def add_patron(person)
+        @patrons << person
+    end
+
+    def yell_at_patrons
+        loud_patrons = @patrons.map do |name|
+            name.upcase 
+        end
+    end
+
+    def over_capacity?
+        if @patrons.length > @capacity
+            true
+        else
+            false
+        end
+    end
+
+    def kick_out
+        @patrons.pop
+    end
 end
