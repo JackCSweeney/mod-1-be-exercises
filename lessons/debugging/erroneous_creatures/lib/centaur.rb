00570d88a9971c9690d7584da8fd80cab7dde372
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-02 10:35:39 -0800

    Add code to finish exercise

--- lessons/debugging/erroneous_creatures/lib/centaur.rb
+++ lessons/debugging/erroneous_creatures/lib/centaur.rb
@@ -3,12 +3,13 @@ class Centaur
   attr_reader :name, :breed
   attr_accessor :crankiness, :standing, :laying, :sleeping
 
-  def initialize(name, breed, standing=true, laying=false, sleeping=false)
+  def initialize(name, breed, standing=true, laying=false, sleeping=false, crankiness = 0)
     @name = name
     @breed = breed
     @standing = standing
     @laying = laying
     @sleeping = sleeping
+    @crankiness = crankiness
   end
 
   def shoot
@@ -31,9 +32,9 @@ class Centaur
 
   def cranky?
     if @crankiness < 3
-      "false"
+      false
     else
-      "true"
+      true
     end
   end
 
@@ -42,7 +43,7 @@ class Centaur
   end
 
   def sleep
-    if @standing
+    if @standing == true
       "NO!"
     else
       @sleeping = true
