682eddb60d6ef50f4c1304fbee63c68ce12597f5
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-02 13:08:59 -0800

    Add menu method and helper method get all names from category

--- practice_independent_challenges/week2/potluck/lib/potluck.rb
+++ practice_independent_challenges/week2/potluck/lib/potluck.rb
@@ -5,6 +5,7 @@ class Potluck
     def initialize(date)
         @date = date
         @dishes = []
+        @menu = {}
     end
 
     def add_dish(dish)
@@ -20,4 +21,23 @@ class Potluck
         end
         dishes_from_category
     end
+
+    def get_all_names_from_category(category)
+        names_from_category = []
+        @dishes.each do |dish|
+            if dish.category == category
+                names_from_category << dish.name
+            end
+        end
+        names_from_category
+    end
+
+    def menu
+        @dishes.each do |dish|
+            @menu[dish.category] = get_all_names_from_category(dish.category).sort
+        end
+        @menu
+    end
+
+
 end
