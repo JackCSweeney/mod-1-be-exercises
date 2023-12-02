ee62b70c581b4a1ac74096880d7545549ba89c12
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-02 12:28:59 -0800

    Add tests to check additional functionality of potluck class

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -46,4 +46,59 @@ RSpec.describe Potluck do
 
         expect(potluck.dishes.length).to eq(2)
     end
+
+    it 'can tell you dishes from specific categories' do
+        potluck = Potluck.new("7-13-18")
+
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        roast_pork = Dish.new("Roast Pork", :entre)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        candy_salad = Dish.new("Candy Salad", :dessert)
+
+        potluck.add_dish(couscous_salad)
+        potluck.add_dish(summer_pizza)
+        potluck.add_dish(roast_pork)
+        potluck.add_dish(cocktail_meatballs)
+        potluck.add_dish(candy_salad)
+
+        expect(potluck.get_all_from_category(:appetizer)).to eq([couscous_salad, summer_pizza])
+        expect(potluck.get_all_from_category(:dessert)).to eq([candy_salad])
+    end
+
+    it 'can tell you the first dish' do
+        potluck = Potluck.new("7-13-18")
+
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        roast_pork = Dish.new("Roast Pork", :entre)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        candy_salad = Dish.new("Candy Salad", :dessert)
+
+        potluck.add_dish(couscous_salad)
+        potluck.add_dish(summer_pizza)
+        potluck.add_dish(roast_pork)
+        potluck.add_dish(cocktail_meatballs)
+        potluck.add_dish(candy_salad)
+
+        expect(potluck.get_all_from_category(:appetizer).first).to eq([couscous_salad, summer_pizza])
+    end
+
+    it 'can tell you the name of the first dish' do
+        potluck = Potluck.new("7-13-18")
+
+        couscous_salad = Dish.new("Couscous Salad", :appetizer)
+        summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        roast_pork = Dish.new("Roast Pork", :entre)
+        cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+        candy_salad = Dish.new("Candy Salad", :dessert)
+
+        potluck.add_dish(couscous_salad)
+        potluck.add_dish(summer_pizza)
+        potluck.add_dish(roast_pork)
+        potluck.add_dish(cocktail_meatballs)
+        potluck.add_dish(candy_salad)
+
+        expect(potluck.get_all_from_category(:appetizer).first.name).to eq([couscous_salad, summer_pizza])
+    end
 end
