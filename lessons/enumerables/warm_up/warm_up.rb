aa53527e95eaac51fcdcc0dc2fba9220cafd447e
Jack Sweeney <jackcsweeney@gmail.com>
2023-12-06 08:56:01 -0800

    Complete exercises

--- lessons/enumerables/warm_up/warm_up.rb
+++ lessons/enumerables/warm_up/warm_up.rb
@@ -4,7 +4,11 @@
 kardashians = ["kris", "kim", "kourtney", "khloe", "rob"]
 
 def upcase_names(names)
- # your code here
+    upper_names = []
+    names.each do |name|
+        upper_names << name.upcase
+    end
+    upper_names
 end 
 
 p upcase_names(kardashians)
@@ -22,7 +26,13 @@ p upcase_names(kardashians)
 pets = ["Brutus", "Lucky", "Goldie", "Pepper", "Odie"]
 
 def names_ending_in_ie(names)
- # your code here
+    ends_with_ie = []
+    names.each do |name|
+        if name.end_with?("ie")
+            ends_with_ie << name
+        end
+    end
+    ends_with_ie
 end 
 
 p names_ending_in_ie(pets)
@@ -40,7 +50,11 @@ p names_ending_in_ie(pets)
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 def first_num_thats_square_is_above_100(nums)
-
+    nums.each do |num|
+        if num * num > 100
+            return num
+        end
+    end
 end 
 
 p first_num_thats_square_is_above_100(nums)
